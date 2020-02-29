61a4bf96c10e4bdceea3ce58fcd21cd09faf2941
Jane <jane.e.nath@gmail.com>
2020-02-29 01:12:06 -0800

    remove axios

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,4 @@
 import history from '../history'
-import axios from 'axios'
 
 export const LOGIN_REQUEST = 'LOGIN_REQUEST'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
@@ -20,9 +19,9 @@ export function loginUser(creds) {
     }
     return dispatch => {
         dispatch(requestLogin(creds))
-        return axios('https://fourth-trimester-api.herokuapp.com/authenticate', config)
+        return fetch('https://fourth-trimester-api.herokuapp.com/authenticate', config)
         .then(response => {
-
+          
             return response.json().then(user => ({ user, response }))
         }).then(({ user, response }) => {
                 if(!response.ok) {
