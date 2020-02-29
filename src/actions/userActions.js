db5bba06a2653614736186d368e1248152dbbe35
Jane <jane.e.nath@gmail.com>
2020-02-29 01:14:54 -0800

    switch to other api

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -19,7 +19,7 @@ export function loginUser(creds) {
     }
     return dispatch => {
         dispatch(requestLogin(creds))
-        return fetch('https://fourth-trimester-api.herokuapp.com/authenticate', config)
+        return fetch('https://fourth-trimester.herokuapp.com/authenticate', config)
         .then(response => {
           
             return response.json().then(user => ({ user, response }))
@@ -98,7 +98,7 @@ export function signupUser(creds) {
   }
   return dispatch => {
       dispatch(requestSignup(creds))
-      return fetch('https://fourth-trimester-api.herokuapp.com/authenticate', config)
+      return fetch('https://fourth-trimester.herokuapp.com/authenticate', config)
       .then(response =>
           response.json().then(user => ({ user, response }))
           ).then(({ user, response }) => {
